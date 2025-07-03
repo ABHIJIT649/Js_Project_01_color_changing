96e956ac224c457c4c2a37096fd0a962f7fd01de
ABHIJIT649 <abhijitmohanty440@gmail.com>
2025-07-03 11:38:36 +0530

    add

--- project_01_color_change/script.js
+++ project_01_color_change/script.js
@@ -8,6 +8,9 @@ buttons.forEach(  function(button){
     button.addEventListener('click', function(e) {
     console.log(e);
     console.log(e.target);
+    
+    //use if statement use 
+
 //    if (e.target.id === 'gray') {
 //     body.style.backgroundColor = e.target.id
 //    }
